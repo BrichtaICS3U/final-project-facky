6caea15e1ec1cf80a782ddca9a53fbac64a9a164
Farhan Mahamud <farhanmahamud@Farhans-MacBook-Air.local>
2018-05-28 10:11:55 -0400

    Added sprites

--- GamePlay.py
+++ GamePlay.py
@@ -159,28 +159,6 @@ while carryOn:
     # Find the distance between player and AOE
     distance = math.hypot (x1 - x2, y1 - y2)
 
-    # Checks pressed Q
-    if distance < staffAOE.radius and active == True :
-        # Checks pressed SpaceBar
-        if keys [pygame.K_SPACE] :
-
-            player.moveLeft (0)
-            player.moveRight (0)
-            player.moveUp (0)
-            player.moveDown (0)
-
-            # Gets position of mouse
-            pos = pygame.mouse.get_pos()
-            xMouse = pos[0]
-            yMouse = pos[1]
-
-            # Creates a "Fireball"
-            fireBall = FireBall (player.rect.x, player.rect.y, xMouse, yMouse)
-
-            # Add fireball to lists
-            spriteList.add (fireBall)
-            projectileList.add (fireBall)
-
     if distance < staffAOE.radius and active == True :
         # Checks pressed SpaceBar
         if keys [pygame.K_SPACE] :
@@ -205,11 +183,7 @@ while carryOn:
                 cooled = False
                 
                 # Start cooldown timer
-<<<<<<< HEAD
-                pygame.time.set_timer (cooldownEvent, 1500)
-=======
                 pygame.time.set_timer (cooldownEvent, 3000)
->>>>>>> d50149e6a2d34b6604873afd8b3d8f26542f1e1c
 
     # Check for every fireball projectile
     for fireball in projectileList :
