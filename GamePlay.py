6bc678109916a879beb140bcde7ef0e4e2221492
Jacky <jlian5@ocdsb.ca>
2018-05-28 11:32:22 -0400

    Revert "going to make game func"
    This reverts commit 1d89312e2d0bce1a259c5102de6127aaa25e4679.

--- GamePlay.py
+++ GamePlay.py
@@ -10,6 +10,7 @@ from GameClasses import StaffAOE
 from GameClasses import Staff
 from GameClasses import Enemy
 from GameClasses import FireBall
+<<<<<<< HEAD
     # Import the pygame library and initialise the game engine
     import pygame, math
 
@@ -19,6 +20,10 @@ pygame.init()
     from GameClasses import Staff
     from GameClasses import Enemy
     from GameClasses import FireBall
+=======
+
+pygame.init()
+>>>>>>> parent of 1d89312... going to make game func
 
 # Define some colours
 # Colours are defined using RGB values
@@ -30,6 +35,7 @@ PURPLE = (174, 20, 188)
 
 # Open a new window
 # The window is defined as (width, height), measured in pixels
+<<<<<<< HEAD
     # Define some colours
     # Colours are defined using RGB values
     BLACK = (0, 0, 0)
@@ -42,12 +48,20 @@ screenW = 1400
 screenH = 785
     # Open a new window
     # The window is defined as (width, height), measured in pixels
+=======
+
+screenW = 1400
+screenH = 785
+>>>>>>> parent of 1d89312... going to make game func
 
 size = (screenW, screenH)
 screen = pygame.display.set_mode (size)
 pygame.display.set_caption("Demon Staff")
+<<<<<<< HEAD
     screenW = 1400
     screenH = 785
+=======
+>>>>>>> parent of 1d89312... going to make game func
 
 # Create lists
 spriteList = pygame.sprite.Group ()
@@ -55,46 +69,62 @@ enemyList = pygame.sprite.Group ()
 objectList = pygame.sprite.Group ()
 projectileList = pygame.sprite.Group ()
 itemList = pygame.sprite.Group ()
+<<<<<<< HEAD
     size = (screenW, screenH)
     screen = pygame.display.set_mode (size)
     pygame.display.set_caption("Demon Staff")
+=======
+>>>>>>> parent of 1d89312... going to make game func
 
 # Create the objects and sets properties
 player = Player (GREEN, 50, 50, 100)
 player.rect.center = (screenW//2, screenH//2)
+<<<<<<< HEAD
     # Create lists
     spriteList = pygame.sprite.Group ()
     enemyList = pygame.sprite.Group ()
     objectList = pygame.sprite.Group ()
     projectileList = pygame.sprite.Group ()
     itemList = pygame.sprite.Group ()
+=======
+>>>>>>> parent of 1d89312... going to make game func
 
 badBoi = Enemy (RED, 0, 0, 100, 4)
 badBoi.rect.x = 1400
 badBoi.rect.y = screenH/2
+<<<<<<< HEAD
     # Create the objects and sets properties
     player = Player (GREEN, 50, 50, 100)
     player.rect.center = (screenW//2, screenH//2)
+=======
+>>>>>>> parent of 1d89312... going to make game func
 
 staff = Staff (PURPLE, 0, 0)
 staff.rect.x = screenW//3
 staff.rect.y = screenH//2
+<<<<<<< HEAD
     badBoi = Enemy (RED, 0, 0, 100, 4)
     badBoi.rect.x = 1400
     badBoi.rect.y = screenH/2
+=======
+>>>>>>> parent of 1d89312... going to make game func
 
 staffAOE = StaffAOE (PURPLE, 400, 400, 400//2)
 staffAOE.rect.x = staff.rect.x - 170
 staffAOE.rect.y = staff.rect.y - 160
+<<<<<<< HEAD
     staff = Staff (PURPLE, 0, 0)
     staff.rect.x = screenW//3
     staff.rect.y = screenH//2
+=======
+>>>>>>> parent of 1d89312... going to make game func
 
 # Put objects into lists
 spriteList.add (player)
 spriteList.add (badBoi)
 spriteList.add (staff)
 spriteList.add (staffAOE)
+<<<<<<< HEAD
     staffAOE = StaffAOE (PURPLE, 400, 400, 400//2)
     staffAOE.rect.x = staff.rect.x - 170
     staffAOE.rect.y = staff.rect.y - 160
@@ -119,10 +149,24 @@ itemList.add (staffAOE)
 active = True
     itemList.add (staff)
     itemList.add (staffAOE)
+=======
+
+enemyList.add (badBoi)
+
+objectList.add (staff)
+objectList.add (staffAOE)
+
+itemList.add (staff)
+itemList.add (staffAOE)
+
+# Check if staff placed or not
+active = True
+>>>>>>> parent of 1d89312... going to make game func
 
 # A cooldown event and boolean to see it's state
 cooldownEvent = pygame.USEREVENT
 cooled = True
+<<<<<<< HEAD
     # Check if staff placed or not
     active = True
 
@@ -140,6 +184,16 @@ clock = pygame.time.Clock ()
 #---------Main Program Loop----------
     # The clock will be used to control how fast the screen updates
     clock = pygame.time.Clock ()
+=======
+
+# This loop will continue until the user exits the game
+carryOn = True
+
+# The clock will be used to control how fast the screen updates
+clock = pygame.time.Clock ()
+
+#---------Main Program Loop----------
+>>>>>>> parent of 1d89312... going to make game func
 
 while carryOn:
     
@@ -161,9 +215,12 @@ while carryOn:
         player.moveUp (5)
     if keys [pygame.K_s] :
         player.moveDown (5)
+<<<<<<< HEAD
     #---------Main Program Loop----------
 
     while carryOn:
+=======
+>>>>>>> parent of 1d89312... going to make game func
         
     # --- Game logic goes here
     spriteList.update ()
@@ -216,6 +273,7 @@ while carryOn:
 
     # Find the distance between player and AOE
     distance = math.hypot (x1 - x2, y1 - y2)
+<<<<<<< HEAD
             
     if distance < staffAOE.radius and active == True :
         # Checks pressed SpaceBar
@@ -239,6 +297,13 @@ while carryOn:
         if keys [pygame.K_s] :
             player.moveDown (5)
             
+=======
+            
+    if distance < staffAOE.radius and active == True :
+        # Checks pressed SpaceBar
+        if keys [pygame.K_SPACE] :
+            
+>>>>>>> parent of 1d89312... going to make game func
             #  - Cooldown portion,
             # based off: https://stackoverflow.com/questions/23368999/move-an-object-every-few-seconds-in-pygame?noredirect=1&lq=1
             if cooled:
@@ -247,6 +312,7 @@ while carryOn:
                 pos = pygame.mouse.get_pos ()
                 xMouse = pos[0]
                 yMouse = pos[1]
+<<<<<<< HEAD
 
                 # Creates a "Fireball"
                 fireBall = FireBall (player.rect.x, player.rect.y, xMouse, yMouse)
@@ -356,71 +422,62 @@ pygame.quit()
         if distance < staffAOE.radius and active == True :
             # Checks pressed SpaceBar
             if keys [pygame.K_SPACE] :
+=======
+
+                # Creates a "Fireball"
+                fireBall = FireBall (player.rect.x, player.rect.y, xMouse, yMouse)
+
+                # Add fireball to lists
+                spriteList.add (fireBall)
+                projectileList.add (fireBall)
+
+                # Now on cooldown
+                cooled = False
+>>>>>>> parent of 1d89312... going to make game func
                 
-                #  - Cooldown portion,
-                # based off: https://stackoverflow.com/questions/23368999/move-an-object-every-few-seconds-in-pygame?noredirect=1&lq=1
-                if cooled:
-                    
-                    # Gets position of mouse
-                    pos = pygame.mouse.get_pos ()
-                    xMouse = pos[0]
-                    yMouse = pos[1]
+                # Start cooldown timer
+                pygame.time.set_timer (cooldownEvent, 3000)
 
-                    # Creates a "Fireball"
-                    fireBall = FireBall (player.rect.x, player.rect.y, xMouse, yMouse)
+    # Check for every fireball projectile
+    for fireball in projectileList :
 
-                    # Add fireball to lists
-                    spriteList.add (fireBall)
-                    projectileList.add (fireBall)
+        # Creates a collision list for enemies 
+       enemyKillList = pygame.sprite.spritecollide (fireball, enemyList, False)
+      
+        # When hits enemy, removes fireball enemies
+       for badboi in enemyKillList :
 
-                    # Now on cooldown
-                    cooled = False
-                    
-                    # Start cooldown timer
-                    pygame.time.set_timer (cooldownEvent, 3000)
+           badboi.health -= 25
+           print (badboi.health)
+           
+           projectileList.remove (fireBall)
+           spriteList.remove (fireBall)
+           
+           if badboi.health <= 0 :
+               badboi.kill ()
 
-        # Check for every fireball projectile
-        for fireball in projectileList :
+        # When goes off screen, remove fireball
+       if fireball.rect.x < 0 or fireball.rect.x > screenW or fireball.rect.y < 0 or fireball.rect.y > screenH :
 
-            # Creates a collision list for enemies 
-           enemyKillList = pygame.sprite.spritecollide (fireball, enemyList, False)
-          
-            # When hits enemy, removes fireball enemies
-           for badboi in enemyKillList :
+           fireball.kill ()
+           
+    # --- Draw code goes here
 
-               badboi.health -= 25
-               print (badboi.health)
-               
-               projectileList.remove (fireBall)
-               spriteList.remove (fireBall)
-               
-               if badboi.health <= 0 :
-                   badboi.kill ()
+    # - Clear the screen to white
+    screen.fill(WHITE)
 
-            # When goes off screen, remove fireball
-           if fireball.rect.x < 0 or fireball.rect.x > screenW or fireball.rect.y < 0 or fireball.rect.y > screenH :
+    # Queue different shapes and lines to be drawn
+    spriteList.draw (screen)
 
-               fireball.kill ()
-               
-        # --- Draw code goes here
+    # - Health Bar 
+    pygame.draw.rect (screen, BLACK, [5, 5, 210, 60], 10)
+    pygame.draw.rect (screen, GREEN, [10, 10, player.health * 2, 50])
 
-        # - Clear the screen to white
-        screen.fill(WHITE)
+    # Update the screen with queued shapes
+    pygame.display.flip()
 
-        # Queue different shapes and lines to be drawn
-        spriteList.draw (screen)
+    # --- Limit to 60 frames per second
+    clock.tick(120)
 
-        # - Health Bar 
-        pygame.draw.rect (screen, BLACK, [5, 5, 210, 60], 10)
-        pygame.draw.rect (screen, GREEN, [10, 10, player.health * 2, 50])
-
-        # Update the screen with queued shapes
-        pygame.display.flip()
-
-        # --- Limit to 60 frames per second
-        clock.tick(120)
-
-    # Once the main program loop is exited, stop the game engine
-    pygame.quit()
-
-Game()
+# Once the main program loop is exited, stop the game engine
+pygame.quit()
